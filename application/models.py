3a70131ce8f29eb97fc4d47154887564fd6ba75f
ASafi99 <56398402+ASafi99@users.noreply.github.com>
2021-12-30 18:40:29 +0000

    Feature/user (#4)
    * adding and displaying users now available - need to link to entity left
    
    * completed feature - added relationships and DB links

--- application/models.py
+++ application/models.py
@@ -1,19 +1,38 @@
 from datetime import datetime
+
+from sqlalchemy.orm import backref
 from application import app, db
 
+class Users(db.Model):
+    id = db.Column(db.Integer, autoincrement=True, primary_key = True)
+    name = db.Column(db.String(80))
+    email = db.Column(db.String(120))
+    wallet = db.Column(db.Integer())
+    expenses = db.relationship('Transactions', backref='users')
+    
+    def __init__(self,name,email,wallet):
+        self.name = name
+        self.email = email
+        self.wallet = wallet
+        
+
+
 class Transactions(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, autoincrement=True, primary_key = True)
     date = db.Column(db.String(100))
     amount = db.Column(db.Integer())
     paytype = db.Column(db.String(100))
     category = db.Column(db.String(100))
     notes = db.Column(db.String(100))
+    expense_userid = db.Column(db.Integer, db.ForeignKey('users.id'),nullable=False)
  
  
-    def __init__(self,date, amount,paytype,category,notes):
+    def __init__(self,date, amount,paytype,category,notes,expense_userid):
  
         self.date = date
         self.amount = amount
         self.paytype= paytype
         self.category= category
         self.notes= notes
+        self.expense_userid = expense_userid
+
--- application/routes.py
+++ application/routes.py
@@ -1,15 +1,64 @@
-from flask import render_template, request, redirect, session,url_for
+from flask import render_template, request, redirect, session,url_for, session
 from flask.helpers import flash
-from application.models import Transactions
+from application.models import Transactions, Users
 from application import app,db
 
 @app.route('/')
-def Index(): 
-    all_data = Transactions.query.all()
-    return render_template("index.html", expenses = all_data)
+@app.route('/users')
+def User_page(): 
+    count_users = Users.query.count()
+    if count_users==0: 
+        return render_template("users.html")
+    else: 
+        all_users = Users.query.all()
+        return render_template("users.html", all_users = all_users)
 
-@app.route('/add',methods=['POST'])
+@app.route('/addUsers',methods=['POST'])
+def addUser(): 
+    name = request.form['name']
+    email = request.form['email']
+    wallet = request.form['wallet']
+    
+    my_data = Users(name,email,wallet)
+
+    db.session.add(my_data)
+    db.session.commit()
+
+    print(name +  " " + email + " " + wallet )
+
+    flash('User has been successfully added!')
+
+    return redirect(url_for('User_page'))
+
+@app.route('/expenses')
+def Transaction(): 
+    userid = request.values.get("userid")
+    user_name = request.values.get("name")
+
+    if userid ==None: 
+        return redirect (url_for("User_page"))
+    else: 
+        expenses_user = db.session.query(Transactions).join(Users).filter(Users.id ==userid).all()
+       
+
+        if expenses_user==[]: 
+            return render_template("expenses.html", name_user = user_name, userid=userid)
+        else: 
+            query = Transactions.query.filter (Transactions.expense_userid== userid).all()
+
+            Total_amount = 0
+            for data in query: 
+                data.amount
+                Total_amount +=data.amount
+            total_amount = round(Total_amount,2)
+
+            return render_template("expenses.html", expenses=expenses_user, name_user = user_name, userid=userid, total =total_amount) 
+
+@app.route('/addExpenses',methods=['POST'])
 def addTransaction():
+
+    userid = request.form["id"]
+  
     
     date = request.form['date']
     amount = request.form['amount']
@@ -17,11 +66,27 @@ def addTransaction():
     category = request.form['category']
     notes = request.form['notes']
     
-    my_data = Transactions(date,amount,paytype,category,notes)
+    my_data = Transactions(date,amount,paytype,category,notes,userid)
     db.session.add(my_data)
     db.session.commit()
     print(date +  " " + amount + " " + paytype + " " + category)
 
     flash('Transaction has been successfully added!')
-    return redirect(url_for('Index'))
+    return redirect(url_for('Transaction'))
+
+
+# @app.route('/update', methods = ['GET', 'POST'])
+# def update():
+ 
+#     if request.method == 'POST':
+#         my_data = Data.query.get(request.form.get('id'))
+ 
+#         my_data.name = request.form['name']
+#         my_data.email = request.form['email']
+#         my_data.phone = request.form['phone']
+ 
+#         db.session.commit()
+#         flash("Employee Updated Successfully")
+ 
+#         return redirect(url_for('Index'))    
 
