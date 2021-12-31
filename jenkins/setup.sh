073083bc2f15842f7e585b782c2bb0be26ee5dc5
Ubuntu <safi-jenkins@dev-jenkins.cigjp2y4eejehkafgoqqtthdxf.cwx.internal.cloudapp.net>
2021-12-31 17:26:31 +0000

    modified files to run app in virtual env

--- jenkins/setup.sh
+++ jenkins/setup.sh
@@ -2,6 +2,19 @@
 
 echo "Setup stage"
 
+ install apt dependencies
+sudo apt update 
+sudo apt install python3 python3-venv python3-pip -y
+
+#create and activate venv
+python3 -m venv venv
+source venv/bin/activate
+
+# install pip requirements
+pip3 install -r requirements.txt
+
+python3 app.py
+
 # apt dependencies
 sudo apt-get update
 sudo apt install -y curl jq
