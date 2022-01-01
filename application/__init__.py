b2792640e8a55ab0b8fe56099b444663acd2627e
Ubuntu <safi-jenkins@dev-jenkins.wfn1tjpbm22uxay05kmk4qigcc.fx.internal.cloudapp.net>
2022-01-01 23:48:58 +0000

    added database uri creds to pipeline

--- application/__init__.py
+++ application/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import getenv
 import uuid
 
 app = Flask(__name__,
@@ -7,7 +8,8 @@ static_url_path='/flask-webapp-project/application/static',
 template_folder='templates'
 )
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
+# app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = getenv("DATABASE_URI")
 app.config['SECRET_KEY'] = str(uuid.uuid4()) # generating a random secret key at app startup
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
