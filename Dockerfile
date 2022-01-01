50f4bd03040fbefaf50ec5d4971d858ae3bb627c
Ubuntu <safi-jenkins@dev-jenkins.wfn1tjpbm22uxay05kmk4qigcc.fx.internal.cloudapp.net>
2022-01-01 15:56:59 +0000

    mod file

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,6 @@ COPY . .
 RUN pip install -r requirements.txt
 # State the listening port for the container. 
 # The app's code does not actually specify the port, so it would be useful to include here.
-EXPOSE 80
+EXPOSE 5000
 # Run 'python app.py' on container start-up. This is the main process.
 ENTRYPOINT ["python", "app.py"]
--- application/__init__.py
+++ application/__init__.py
@@ -10,7 +10,6 @@ template_folder='templates'
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
 app.config['SECRET_KEY'] = str(uuid.uuid4()) # generating a random secret key at app startup
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 db = SQLAlchemy(app)
 from application import routes
