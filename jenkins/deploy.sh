bab1684100f9a56767f5eff9ed128516e7182daa
Ubuntu <safi-jenkins@dev-jenkins.wfn1tjpbm22uxay05kmk4qigcc.fx.internal.cloudapp.net>
2022-01-01 04:37:29 +0000

    mod file

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -2,5 +2,6 @@
 
 echo "Deploy stage"
 
-scp docker-compose.yaml jenkins@swarm-manager:/home/jenkins/docker-compose.yaml
-ssh jenkins@swarm-manager DOCKER_HUB_CREDS_USR=$DOCKER_HUB_CREDS_USR docker stack deploy --compose-file docker-compose.yaml expense-tracker
+scp docker-compose.yaml jenkins@DeploymentServer:/home/jenkins/docker-compose.yaml
+ssh jenkins@DeploymentServer
+DOCKER_HUB_CREDS_USR=$DOCKER_HUB_CREDS_USR docker stack deploy --compose-file docker-compose.yaml expense-tracker
