1b26f1e11625b6393f9e80cd1c50daa79ef2d9ba
Ubuntu <safi-jenkins@dev-jenkins.wfn1tjpbm22uxay05kmk4qigcc.fx.internal.cloudapp.net>
2022-01-02 03:33:09 +0000

    added test stage publish reports

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -14,7 +14,7 @@ mkdir test-reports
 
 # run pytest
 python3 -m pytest \
-    --cov/application \
+    --cov=application \
     --cov-report term missing \
     --cov-report xml:test_reports/coverage.xml \
     --junitxml=test_reports/junit_report.xml
