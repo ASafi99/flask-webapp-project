28bac91b0e29d91036d900b8ec1e004c0c251986
ASafi99 <56398402+ASafi99@users.noreply.github.com>
2022-01-02 04:17:59 +0000

    Feature/tests (#10)
    * added tests to 90% coverage
    
    * added database uri creds to pipeline
    
    * jenkins detect this change
    
    * added test stage publish reports
    
    * added test stage publish reports
    
    Co-authored-by: Ubuntu <safi-jenkins@dev-jenkins.wfn1tjpbm22uxay05kmk4qigcc.fx.internal.cloudapp.net>

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ def create_tables():
     db.create_all()
 
 if __name__ == "__main__":
-    app.run(host = '0.0.0.0')
+    app.run(host = '0.0.0.0',debug=True)
--- jenkins/test.sh
+++ jenkins/test.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
 echo "Test stage"
+
+# venv created, source
+python3 -m venv venv
+source venv/bin/activate
+
+# install pip dependencies
+pip3 install pytest pytest-cov flask_testing
+pip3 install -r requirements.txt
+
+mkdir test_reports
+
+# run pytest
+python3 -m pytest \
+    --cov=application \
+    --cov-report term-missing \
+    --cov-report xml:test_reports/coverage.xml \
+    --junitxml=test_reports/junit_report.xml
+
+
+# remove venv
+deactivate
+rm -rf venv
