979d876b02d3c7d7b0a0b94cf9ab7a828e38a767
Ubuntu <safi-jenkins@dev-jenkins.wfn1tjpbm22uxay05kmk4qigcc.fx.internal.cloudapp.net>
2022-01-02 01:55:25 +0000

    jenkins detect this change

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 from application import app, db
 from application.models import Users
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
 
 if __name__ == "__main__":
     app.run(host = '0.0.0.0',debug=True)
