74d2fca78cb58d95ff34406e02cedcdb90ffa050
Ubuntu <safi-jenkins@dev-jenkins.wfn1tjpbm22uxay05kmk4qigcc.fx.internal.cloudapp.net>
2022-01-02 03:25:40 +0000

    added test stage publish reports

--- jenkins/setup.sh
+++ jenkins/setup.sh
@@ -18,9 +18,11 @@ sudo apt-get update
 sudo apt install -y curl jq
 
 # install docker
+if [ ! -f "/usr/bin/docker" ]; then
 curl https://get.docker.com | sudo bash
 sudo usermod -aG docker jenkins
 newgrp docker
+fi
 
 # install docker compose
 version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
